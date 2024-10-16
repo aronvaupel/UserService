b607ebee1a60eb1fa089dbd78753f0cb03980352
aron <vaupelaron@gmail.com>
2024-10-16 14:57:32 +0700

    Extract Redis to commons.

--- src/main/kotlin/com/ecommercedemo/userservice/UserServiceApplication.kt
+++ src/main/kotlin/com/ecommercedemo/userservice/UserServiceApplication.kt
@@ -1,9 +1,12 @@
 package com.ecommercedemo.userservice
 
+import com.ecommercedemo.common.redis.RedisConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(RedisConfig::class)
 class UserServiceApplication
 
 fun main(args: Array<String>) {
