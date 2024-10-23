f8d892debc864bbb52780704194e8947a2aad907
aron <vaupelaron@gmail.com>
2024-10-23 14:42:46 +0700

    Update commons, enable scheduling

--- src/main/kotlin/com/ecommercedemo/userservice/UserServiceApplication.kt
+++ src/main/kotlin/com/ecommercedemo/userservice/UserServiceApplication.kt
@@ -2,9 +2,10 @@ package com.ecommercedemo.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(scanBasePackages = ["com.ecommercedemo.common", "com.ecommercedemo.userservice"])
+@EnableScheduling
 class UserServiceApplication
 
 fun main(args: Array<String>) {
