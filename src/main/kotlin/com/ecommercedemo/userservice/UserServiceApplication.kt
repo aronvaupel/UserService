621493a739b74942a7ffc90090876d47c1adb61a
aron <vaupelaron@gmail.com>
2024-10-23 13:28:49 +0700

    Refactor annotation  in main

--- src/main/kotlin/com/ecommercedemo/userservice/UserServiceApplication.kt
+++ src/main/kotlin/com/ecommercedemo/userservice/UserServiceApplication.kt
@@ -4,8 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
-@ComponentScan("com.ecommercedemo.userservice", "com.ecommercedemo.common")
+@SpringBootApplication(scanBasePackages = ["com.ecommercedemo.common", "com.ecommercedemo.userservice"])
 class UserServiceApplication
 
 fun main(args: Array<String>) {
