2827994bdd23a0f543b4d29f641d564c9cd1f85f
aron <vaupelaron@gmail.com>
2025-01-18 19:00:50 +0700

    rm security

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,8 +63,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	//Fixme
-	implementation("org.springframework.boot:spring-boot-starter-security:3.3.4")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	if (!isLocalProfile) {
